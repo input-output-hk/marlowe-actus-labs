9f15b1c9d0c236a7035233c66067f92f13fd2afd
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2022-12-06 14:21:36 +0100

    hello react

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,26 @@
-module Main where
+module Main
+  ( main
+  ) where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Effect.Exception (throw)
+import React.Basic.DOM as React.DOM
+import React.Basic.DOM.Client (createRoot, renderRoot)
+import Web.DOM (Element)
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.HTML (HTMLDocument, window)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
+import Web.HTML.Window (document)
 
 main :: Effect Unit
 main = do
-  pure unit
+  doc :: HTMLDocument <- document =<< window
+  root :: Maybe Element <- getElementById "app-root" $ toNonElementParentNode doc
+  case root of
+    Nothing -> throw "Could not find element with id 'app-root'"
+    Just container -> do
+      reactRoot <- createRoot container
+      renderRoot reactRoot (React.DOM.text "Hello, world!")
