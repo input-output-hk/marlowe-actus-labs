7077e4706f588a2c68e3ff655527fc677868349e
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2022-12-06 14:46:16 +0100

    react spa skeleton

--- src/Main.purs
+++ src/Main.purs
@@ -5,15 +5,36 @@ module Main
 import Prelude
 
 import Data.Maybe (Maybe(..))
+import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Effect.Exception (throw)
 import React.Basic.DOM as React.DOM
 import React.Basic.DOM.Client (createRoot, renderRoot)
+import React.Basic.Events as React.Events
+import React.Basic.Hooks (Component, component)
+import React.Basic.Hooks as React
 import Web.DOM (Element)
 import Web.DOM.NonElementParentNode (getElementById)
 import Web.HTML (HTMLDocument, window)
 import Web.HTML.HTMLDocument (toNonElementParentNode)
 import Web.HTML.Window (document)
+
+mkCounter :: Component Int
+mkCounter = do
+  component "Counter" \initialValue -> React.do
+    counter /\ setCounter <- React.useState initialValue
+    pure $ React.fragment
+      [ React.DOM.text $ "Counter: " <> show counter
+      , React.DOM.br {}
+      , React.DOM.button
+          { onClick: React.Events.handler_ $ setCounter (_ + 1)
+          , children: [ React.DOM.text "Increment" ]
+          }
+      , React.DOM.button
+          { onClick: React.Events.handler_ $ setCounter (_ - 1)
+          , children: [ React.DOM.text "Decrement" ]
+          }
+      ]
 
 main :: Effect Unit
 main = do
@@ -23,4 +44,5 @@ main = do
     Nothing -> throw "Could not find element with id 'app-root'"
     Just container -> do
       reactRoot <- createRoot container
-      renderRoot reactRoot (React.DOM.text "Hello, world!")
+      counter <- mkCounter
+      renderRoot reactRoot (counter 0)
