3108b77671f2cbbb0509739b4513266856f9e96c
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2022-12-06 14:15:17 +0100

    web app entry point

--- src/dev-server.js
+++ src/dev-server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.use( "/", [ express.static("./" ) ] );
+app.use( "/", [ express.static("./public" ) ] );
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
