808192dd01fb32d9db96c1607dd2ae46ba6786c8
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2022-12-08 15:02:59 +0100

    interim

--- src/Main.purs
+++ src/Main.purs
@@ -4,45 +4,46 @@ module Main
 
 import Prelude
 
+import Data.Array as Array
 import Data.Maybe (Maybe(..))
 import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Effect.Exception (throw)
 import React.Basic.DOM as React.DOM
 import React.Basic.DOM.Client (createRoot, renderRoot)
-import React.Basic.Events as React.Events
 import React.Basic.Hooks (Component, component)
 import React.Basic.Hooks as React
+import Wallet (Wallet)
 import Web.DOM (Element)
 import Web.DOM.NonElementParentNode (getElementById)
-import Web.HTML (HTMLDocument, window)
+import Web.HTML (HTMLDocument, Window, window)
 import Web.HTML.HTMLDocument (toNonElementParentNode)
 import Web.HTML.Window (document)
 
-mkCounter :: Component Int
-mkCounter = do
+newtype StartPageState = StartPageState (Array Wallet)
+
+data Page = StartPage StartPageState
+
+actusStartPage :: StartPageState -> React.JSX
+actusStartPage (StartPageState wallets)
+  | Array.null wallets = React.DOM.text "No wallets detected"
+  | otherwise = React.DOM.text "Wallets detected!"
+
+mkActusPrototype :: Component Page
+mkActusPrototype = do
   component "Counter" \initialValue -> React.do
-    counter /\ setCounter <- React.useState initialValue
-    pure $ React.fragment
-      [ React.DOM.text $ "Counter: " <> show counter
-      , React.DOM.br {}
-      , React.DOM.button
-          { onClick: React.Events.handler_ $ setCounter (_ + 1)
-          , children: [ React.DOM.text "Increment" ]
-          }
-      , React.DOM.button
-          { onClick: React.Events.handler_ $ setCounter (_ - 1)
-          , children: [ React.DOM.text "Decrement" ]
-          }
-      ]
+    (page :: Page) /\ (_ :: Page -> Effect Unit) <- React.useState' initialValue
+    case page of
+      StartPage startPageState -> pure $ actusStartPage startPageState
 
 main :: Effect Unit
 main = do
-  doc :: HTMLDocument <- document =<< window
+  w :: Window <- window
+  doc :: HTMLDocument <- document w
   root :: Maybe Element <- getElementById "app-root" $ toNonElementParentNode doc
   case root of
     Nothing -> throw "Could not find element with id 'app-root'"
     Just container -> do
       reactRoot <- createRoot container
-      counter <- mkCounter
-      renderRoot reactRoot (counter 0)
+      actusPrototype <- mkActusPrototype
+      renderRoot reactRoot $ actusPrototype $ StartPage $ StartPageState []
