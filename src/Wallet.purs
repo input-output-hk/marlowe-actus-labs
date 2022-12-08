503c91d274983b81e8a859412d476980d1a50a59
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2022-12-08 14:59:48 +0100

    better naming

--- src/Wallet.purs
+++ src/Wallet.purs
@@ -4,9 +4,11 @@ module Wallet
   , Cardano
   , Icon(..)
   , Name(..)
+  , Wallet(..)
   , apis
   , cardano
-  ) where
+  )
+  where
 
 import Prelude
 
@@ -31,10 +33,11 @@ import Web.HTML (Window)
 newtype ApiVersion = ApiVersion String
 newtype Icon = Icon String
 newtype Name = Name String
+newtype Api = Api Foreign
 
-data Api = Api
+data Wallet = Wallet
   { apiVersion :: ApiVersion
-  , enable :: Aff Foreign
+  , enable :: Aff Api
   , icon :: Icon
   , isEnabled :: Aff Boolean
   , name :: Name
@@ -56,20 +59,20 @@ cardano w = do
       void $ Foreign.unsafeReadTagged "Object" c
       pure $ Just $ Foreign.unsafeFromForeign c
 
-apis :: Cardano -> Effect (Array Api)
+apis :: Cardano -> Effect (Array Wallet)
 apis c = do
   o :: Object Foreign <- throwForeignErrors $ Foreign.unsafeReadTagged "Object" $ Foreign.unsafeToForeign c
   Array.mapMaybe Either.hush <$> traverse (runExceptT <<< readApi) (Object.values o)
 
-readApi :: forall m. Monad m => Foreign -> ExceptT (NonEmptyList ForeignError) m Api
+readApi :: forall m. Monad m => Foreign -> ExceptT (NonEmptyList ForeignError) m Wallet
 readApi x = do
   void $ Foreign.unsafeReadTagged "Object" x
   apiVersion :: ApiVersion <- readApiVersion =<< Foreign.Index.readProp "apiVersion" x
-  enable :: Aff Foreign <- readEnable =<< Foreign.Index.readProp "enable" x
+  enable :: Aff Api <- readEnable =<< Foreign.Index.readProp "enable" x
   icon :: Icon <- readIcon =<< Foreign.Index.readProp "icon" x
   isEnabled :: Aff Boolean <- readIsEnabled =<< Foreign.Index.readProp "isEnabled" x
   name :: Name <- readName =<< Foreign.Index.readProp "name" x
-  pure $ Api { apiVersion, enable, icon, isEnabled, name }
+  pure $ Wallet { apiVersion, enable, icon, isEnabled, name }
   where
   readApiVersion :: Foreign -> ExceptT (NonEmptyList ForeignError) m ApiVersion
   readApiVersion = map ApiVersion <<< Foreign.readString
@@ -80,9 +83,9 @@ readApi x = do
   readName :: Foreign -> ExceptT (NonEmptyList ForeignError) m Name
   readName = map Name <<< Foreign.readString
 
-  readEnable :: Foreign -> ExceptT (NonEmptyList ForeignError) m (Aff Foreign)
+  readEnable :: Foreign -> ExceptT (NonEmptyList ForeignError) m (Aff Api)
   readEnable x' = do
-    f :: Effect (Promise Foreign) <- Foreign.unsafeReadTagged "Function" x'
+    f :: Effect (Promise Api) <- Foreign.unsafeReadTagged "Function" x'
     arity :: Int <- Foreign.readInt =<< Foreign.Index.readProp "length" x'
     unless (arity == 0) $ Foreign.fail $ ForeignError "Expected procedure of arity 0"
     pure $ toAffE f
