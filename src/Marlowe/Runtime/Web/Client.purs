6ea2e7e366e58d4cf1bf394df20a663c9314e46b
Yves Hauser <yves.hauser@gmail.com>
2022-12-09 17:40:31 +0100

    SCP-4791: Initial client

--- src/Marlowe/Runtime/Web/Client.purs
+++ src/Marlowe/Runtime/Web/Client.purs
@@ -2,24 +2,74 @@ module Marlowe.Runtime.Web.Client where
 
 import Prelude
 
+import Control.Monad.Error.Class (class MonadThrow)
 import Control.Monad.Except (throwError)
-import Data.Argonaut (class DecodeJson, Json, decodeJson, printJsonDecodeError)
-import Data.Either (Either, either)
-import Effect.Aff (Aff, error)
+import Data.Argonaut (class DecodeJson, Json, JsonDecodeError, decodeJson, fromObject, fromString, printJsonDecodeError, stringify)
+import Data.Argonaut.Decode ((.:))
+import Data.Either (Either(..), either)
+import Data.Traversable (for)
+import Effect.Aff (Aff, Error, error)
 import Fetch (fetch)
-import Fetch.Argonaut.Json (fromJson)
 import Foreign (Foreign)
-import Marlowe.Runtime.Web.Types (ResourceLink(..), ServerURL(..))
+import Foreign.Object as Object
+import Marlowe.Runtime.Web.Types (ContractHeader, ContractState, ResourceLink(..), ResourceWithLinks, ServerURL(..), Tx, TxHeader, decodeResourceWithLink)
 import Unsafe.Coerce (unsafeCoerce)
 
--- fetchConractHeaders ..
--- 
--- fetchContract
--- 
--- fetchTransactionHeaders
--- 
--- fetchTransaction
+-- fetchConractHeaders
+fetchContractHeaders :: ServerURL -> String -> Aff (Array (ResourceWithLinks ContractHeader (contract :: ResourceLink ContractState)))
+fetchContractHeaders serverUrl location = do
+  let
+    res = ResourceLink location
+  json <- fetchResource serverUrl res
+  (contractsWithLinksJson :: Array Json) <- either (throwError <<< error <<< show) pure do
+    obj <- decodeJson json
+    obj .: "results"
 
+  for contractsWithLinksJson \contractWithLinksJson -> do
+      let contract :: Either JsonDecodeError (ResourceWithLinks ContractHeader (contract :: ResourceLink ContractState))
+          contract = decodeResourceWithLink (map decodeJson) contractWithLinksJson
+      handleError contractWithLinksJson contract
+
+-- fetchContract
+fetchContract :: ServerURL -> String -> Aff (ResourceWithLinks ContractState (transactions :: ResourceLink (Array TxHeader)))
+fetchContract serverUrl location = do
+  let
+    res = ResourceLink location
+  json <- fetchResource serverUrl res
+
+  let
+    contractState :: Either JsonDecodeError (ResourceWithLinks ContractState (transactions :: ResourceLink (Array TxHeader)))
+    contractState = decodeResourceWithLink (map decodeJson) json
+  handleError json contractState
+
+-- fetchTransactionHeaders
+fetchTransactionHeaders :: ServerURL -> String -> Aff (Array (ResourceWithLinks TxHeader (transaction :: ResourceLink Tx)))
+fetchTransactionHeaders serverUrl location = do
+  let
+    res = ResourceLink location
+  json <- fetchResource serverUrl res
+  (txHeadersJsonArr :: Array Json) <- either (throwError <<< error <<< show) pure do
+      obj <- decodeJson json
+      obj .: "results"
+
+  for txHeadersJsonArr \txHeaderJson -> do
+    let txHeader :: Either JsonDecodeError (ResourceWithLinks TxHeader (transaction :: ResourceLink Tx))
+        txHeader = decodeResourceWithLink (map decodeJson) txHeaderJson
+    handleError txHeaderJson txHeader
+
+-- fetchTransaction
+fetchTransaction :: ServerURL -> String -> Aff (ResourceWithLinks Tx (previous :: ResourceLink Tx))
+fetchTransaction serverUrl location = do
+  let
+    res = ResourceLink location
+  json <- fetchResource serverUrl res
+
+  let
+    tx :: Either JsonDecodeError (ResourceWithLinks Tx (previous :: ResourceLink Tx))
+    tx = decodeResourceWithLink (map decodeJson) json
+  handleError json tx
+
+-- fetchResource
 fetchResource :: forall a. DecodeJson a => ServerURL -> ResourceLink a -> Aff a
 fetchResource (ServerURL serverUrl) (ResourceLink path) = do
   let
@@ -36,3 +86,9 @@ fetchResource (ServerURL serverUrl) (ResourceLink path) = do
     pure
     (decodeJson json)
 
+-- handleError
+handleError :: forall a b c. Show b => MonadThrow Error a => Json -> Either b c -> a c
+handleError json (Left err) =
+  let errJson = fromObject $ Object.fromHomogeneous { json, err: fromString $ show err }
+   in throwError $ error $ stringify errJson
+handleError _ (Right c) = pure c
