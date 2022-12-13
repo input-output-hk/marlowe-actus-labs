12e49051d31df54afe6c31cdbaefad2623988518
Yves Hauser <yves.hauser@gmail.com>
2022-12-13 17:32:32 +0100

    SCP-4791: Filter contracts

--- src/Component/ContractList.purs
+++ src/Component/ContractList.purs
@@ -2,18 +2,29 @@ module Component.ContractList where
 
 import Prelude
 
+import Data.Array (filter)
+import Data.Map (empty)
 import Data.Newtype (unwrap)
-import Marlowe.Runtime.Web.Types (ContractHeader, ContractState, ResourceLink, ResourceWithLinks)
+import Marlowe.Runtime.Web.Types (ContractHeader(..), ContractState, ResourceLink, ResourceWithLinks)
 import React.Basic.DOM as React.DOM
 import React.Basic.Hooks (JSX)
 import React.Basic.Hooks as React
 
-type ContractList = Array (ResourceWithLinks ContractHeader (contract :: ResourceLink ContractState))
 
-contractList :: ContractList -> JSX
+contractList :: Array (ResourceWithLinks ContractHeader (contract :: ResourceLink ContractState)) -> JSX
 contractList =
-  React.fragment <<< map \{ links } ->
-    React.DOM.div
-      { className: "contracts-list"
-      , children: [ React.DOM.text $ unwrap links.contract ]
-      }
+  React.fragment
+    <<< map
+      ( \{ links } ->
+          React.DOM.div
+            { className: "contracts-list"
+            , children: [ React.DOM.text $ unwrap links.contract ]
+            }
+      )
+    <<< filter
+      ( \{ resource } ->
+          let
+            ContractHeader header = resource
+          in
+            header.metadata == empty -- TODO: find ACTUS contract in Metadata
+      )
