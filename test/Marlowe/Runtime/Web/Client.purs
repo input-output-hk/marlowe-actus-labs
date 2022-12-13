3214ddcd982300beac2cc0eb45ad21d667e1a5b1
Yves Hauser <yves.hauser@gmail.com>
2022-12-13 15:35:53 +0100

    SCP-4791: Initial test case for client module

--- test/Marlowe/Runtime/Web/Client.purs
+++ test/Marlowe/Runtime/Web/Client.purs
@@ -2,21 +2,28 @@ module Test.Marlowe.Web.Client where
 
 import Prelude
 
-import Control.Monad.Error.Class (throwError)
-import Data.Argonaut (Json, JsonDecodeError, decodeJson, fromObject, fromString, jsonParser, stringify)
-import Data.Argonaut.Decode ((.:))
-import Data.Either (Either(..), either)
-import Data.Foldable (for_)
-import Effect.Exception (error)
-import Foreign.Object as Object
-import Marlowe.Runtime.Web.Types (ContractHeader, ContractState, ResourceLink, ResourceWithLinks, ServerURL(..), Tx, TxHeader, decodeResourceWithLink)
-import Node.Encoding (Encoding(..))
-import Node.FS.Aff (readTextFile)
+import Data.Array (head)
+import Data.Maybe (Maybe(..))
+import Marlowe.Runtime.Web.Client (fetchContract, fetchContractHeaders, fetchTransaction, fetchTransactionHeaders)
+import Marlowe.Runtime.Web.Types (ServerURL(..), Tx(..))
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (fail)
 
 spec :: ServerURL -> Spec Unit
 spec serverUrl@(ServerURL serverUrlStr) = do
-  describe ("Testing web client against" <> serverUrlStr) do
-     it "STUB" do
-        pure unit
+  describe ("Testing web client against " <> serverUrlStr) do
+     it "contracts" do
+        contracts <- fetchContractHeaders serverUrl
+        case head contracts of
+            Just contractHeader -> do
+              contract <- fetchContract serverUrl contractHeader.links.contract
+              transactionHeaders <- fetchTransactionHeaders serverUrl contract.links.transactions
+              case head transactionHeaders of
+               Just transactionHeader -> do
+                  transaction <- fetchTransaction serverUrl transactionHeader.links.transaction
+                  let (Tx tx) = transaction.resource
+                  case tx.block of
+                         Just _ -> pure unit
+                         _ -> fail "Expected block"
+               _ -> fail "Expected transaction"
+            _ -> fail "Expected contract"
