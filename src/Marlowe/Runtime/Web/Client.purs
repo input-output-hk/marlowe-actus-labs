7573bd1fc2d5ddd42addbfe17ea7993e9b3c3bd5
Yves Hauser <yves.hauser@gmail.com>
2022-12-14 10:29:18 +0100

    SCP-4791: Fetch more data

--- src/Marlowe/Runtime/Web/Client.purs
+++ src/Marlowe/Runtime/Web/Client.purs
@@ -2,14 +2,22 @@ module Marlowe.Runtime.Web.Client where
 
 import Prelude
 
+import Control.Apply (lift2)
 import Control.Monad.Error.Class (class MonadThrow)
 import Control.Monad.Except (throwError)
 import Data.Argonaut (Json, JsonDecodeError, decodeJson, fromObject, fromString, stringify)
 import Data.Argonaut.Decode ((.:))
+import Data.Array (concat, singleton, (:))
+import Data.Bifunctor (lmap)
 import Data.Either (Either(..), either)
+import Data.Map (Map, fromFoldable, lookup)
+import Data.Maybe (Maybe(..))
+import Data.String.CaseInsensitive (CaseInsensitiveString(..))
 import Data.Traversable (for)
+import Debug (traceM)
 import Effect.Aff (Aff, Error, error)
 import Fetch (fetch)
+import Fetch.Core.Headers (Headers, toArray)
 import Foreign (Foreign)
 import Foreign.Object as Object
 import Marlowe.Runtime.Web.Types (ContractHeader, ContractState, ResourceLink(..), ResourceWithLinks, ServerURL(..), Tx, TxHeader, decodeResourceWithLink)
@@ -19,11 +27,12 @@ import Unsafe.Coerce (unsafeCoerce)
 fetchContractHeaders :: ServerURL -> Aff (Array (ResourceWithLinks ContractHeader (contract :: ResourceLink ContractState)))
 fetchContractHeaders serverUrl = do
   let res = ResourceLink "contracts"
-  json <- fetchResource serverUrl res
-  (contractsWithLinksJson :: Array Json) <- either (throwError <<< error <<< show) pure do
-    obj <- decodeJson json
-    obj .: "results"
-  for contractsWithLinksJson \contractWithLinksJson -> do
+  jsons <- fetchResources serverUrl res
+  (contractsWithLinksJson :: Array (Array Json)) <- either (throwError <<< error <<< show) pure $
+    for jsons \json -> do
+      obj <- decodeJson json
+      obj .: "results"
+  for (concat contractsWithLinksJson) \contractWithLinksJson -> do
       let contract :: Either JsonDecodeError (ResourceWithLinks ContractHeader (contract :: ResourceLink ContractState))
           contract = decodeResourceWithLink (map decodeJson) contractWithLinksJson
       handleError contractWithLinksJson contract
@@ -40,12 +49,13 @@ fetchContract serverUrl res = do
 -- fetchTransactionHeaders
 fetchTransactionHeaders :: ServerURL -> ResourceLink (Array TxHeader) -> Aff (Array (ResourceWithLinks TxHeader (transaction :: ResourceLink Tx)))
 fetchTransactionHeaders serverUrl res = do
-  json <- fetchResource serverUrl res
-  (txHeadersJsonArr :: Array Json) <- either (throwError <<< error <<< show) pure do
+  jsons <- fetchResources serverUrl res
+  (txHeadersJsonArr :: Array (Array Json)) <- either (throwError <<< error <<< show) pure $
+    for jsons \json -> do
       obj <- decodeJson json
       obj .: "results"
 
-  for txHeadersJsonArr \txHeaderJson -> do
+  for (concat txHeadersJsonArr) \txHeaderJson -> do
     let txHeader :: Either JsonDecodeError (ResourceWithLinks TxHeader (transaction :: ResourceLink Tx))
         txHeader = decodeResourceWithLink (map decodeJson) txHeaderJson
     handleError txHeaderJson txHeader
@@ -69,9 +79,38 @@ fetchResource (ServerURL serverUrl) (ResourceLink path) = do
     bringBackJson = unsafeCoerce
 
   res <- fetch url { headers: { "Accept": "application/json" } }
-  -- TODO: check HTTP status code - if 206 get next chunk (via Range)
-
   bringBackJson <$> res.json
+
+-- fetchResources
+fetchResources :: forall a. ServerURL -> ResourceLink a -> Aff (Array Json)
+fetchResources (ServerURL serverUrl) (ResourceLink path) = do
+  let
+    url = serverUrl <> path
+
+    bringBackJson :: Foreign -> Json
+    bringBackJson = unsafeCoerce
+
+    fetchAll :: _ -> Aff (Array Json)
+    fetchAll header = do
+       { json, status, headers } <- fetch url header
+       if status == 206
+         then
+            case lookup (CaseInsensitiveString "Next-Range") (toHeaders headers) of
+               Just nextRange ->
+                  lift2 (:)
+                    (bringBackJson <$> json)
+                    (fetchAll { headers: { "Accept": "application/json" , "Range": nextRange } })
+               _ -> do
+                 traceM json
+                 throwError $ error "HTTP 206 but Next-Range header missing"
+         else
+            singleton <<< bringBackJson <$> json
+
+  fetchAll { headers: { "Accept": "application/json" , "Range": "contractId" } }
+
+-- from: Fetch.Internal.Headers
+toHeaders :: Headers -> Map CaseInsensitiveString String
+toHeaders = toArray >>> map (lmap CaseInsensitiveString) >>> fromFoldable
 
 -- handleError
 handleError :: forall a b c. Show b => MonadThrow Error a => Json -> Either b c -> a c
