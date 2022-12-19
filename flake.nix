df7adf2a7d6c13af88c97fc3fc83d67debe75dc8
Tomasz Rybarczyk <tomasz.rybarczyk@iohk.io>
2022-12-19 12:10:49 +0100

    Update deps

--- flake.nix
+++ flake.nix
@@ -10,17 +10,21 @@
       url = "github:justinwoo/easy-purescript-nix";
     };
   };
+
   outputs = { self, nixpkgs, flakeUtils, easyPSSrc }:
     flakeUtils.lib.eachSystem ["x86_64-linux"] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         easyPS = pkgs.callPackage easyPSSrc { inherit pkgs; };
+        nodejs-16 = pkgs.writeShellScriptBin "nodejs-16" ''
+          ${ pkgs.nodejs-16_x.out}/bin/node $@
+        '';
       in {
         devShell = pkgs.mkShell {
           buildInputs = [
 
             # Please update spago and purescript in `package.json` `scripts` section
-            easyPS."purs-0_15_6"
+            easyPS."purs-0_15_7"
             easyPS.purescript-language-server
             easyPS.pscid
             easyPS.purs-tidy
@@ -38,6 +42,7 @@
             pkgs.nodePackages.webpack-dev-server
             pkgs.dhall
             pkgs.nodejs-18_x
+            nodejs-16
             pkgs.pkgconfig
             pkgs.postgresql
             pkgs.python27
@@ -47,7 +52,7 @@
         ];
         shellHook = ''
           npm install
-          NODE_OPTIONS=--experimental-fetch spago build
+          NODE_OPTIONS=--experimental-fetch --trace-warnings
           export PATH=$PATH:./node_modules/.bin/:./bin
           export PS1="\n\[\033[1;32m\][nix develop:\w]\$\[\033[0m\] ";
         '';
